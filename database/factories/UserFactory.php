8e67fdfa2715fdd6337c957a2e4ecb9d8d757d46
valeu36 <kama4ok111@gmail.com>
2019-11-18 02:12:13 +0200

    factories test(UserFactory.php)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,18 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $role_id = mt_rand(1, 3);
+
     return [
         'name' => $faker->name,
+        'surname' => $faker->lastName,
+        'middle_name' => $faker->word,
+        'nick' => $faker->word,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'is_active' => $faker->boolean,
+        'role_id' => $role_id,
         'remember_token' => Str::random(10),
     ];
 });
